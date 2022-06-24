42f26cd0b6e96335cc31c4a8ae02e90a0763189a
James <jamesballsemail@gmail.com>
2022-06-24 09:49:43 +0100

    setup finish

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: [
+    "./public/index.html",
+    "./public/**/*.{html,js}",
+  ],
   theme: {
     extend: {},
   },
